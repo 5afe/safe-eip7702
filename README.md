006487b148d03fbb24fc899d6cce4f76dcbf2b5e
Akshay <akshay.ap95@gmail.com>
2024-10-01 14:40:18 +0200

    Update readme, tests

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # safe-interoperable-delegated-account-poc
 A POC for ERC-Interoperable Delegated Account
 
+This repository showcases how EOA account can delegate to a Safe account. [ERC-7702](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-7702.md) introduces a new transaction type 4 that allows EOA to set code and delegate to a contract.
+
+[SafeERC7702.sol](./contracts/SafeERC7702.sol) is a custom proxy contract that can be setup as a delegate for an EOA account. A custom proxy is required because ERC-7702 does not allow executing initcode while giving authority to a contract. Having a custom proxy to enforce that account storage can only be initialized with pre-determined and approved initializer.
+
+This proxy contract take hash of setup data in constructor which means that proxy address depends on the setup data. Thus, it is not possible to initialize storage of EOA with arbitrary data after the authorization transaction.
+
 ## Install
 
 ```bash
--- test/ERC7702.spec.ts
+++ test/ERC7702.spec.ts
@@ -9,16 +9,15 @@ import {
     getClearStorageHelper,
     getSafeModuleSetup,
 } from "./utils/setup";
-import { AddressLike, Provider, SigningKey } from "ethers";
+import { AddressLike, SigningKey } from "ethers";
 import { execTransaction, getSetupData, GUARD_STORAGE_SLOT, readModuleStorageSlot, readOwnerStorageSlot } from "../src/utils/safe";
 import { expect } from "chai";
 import { ACCOUNT_CODE_PREFIX, calculateProxyAddress, getAuthorizationList, getSignedTransaction } from "../src/erc7702/helper";
 import { FALLBACK_HANDLER_STORAGE_SLOT, SENTINEL_ADDRESS } from "../src/utils/safe";
 import { SafeERC7702ProxyFactory } from "../typechain-types";
-import { readStorage } from "../src/utils/storageReader";
 import { isAccountDelegatedToAddress } from "../src/erc7702/storage";
 
-describe("FallbackHandler", () => {
+describe("ERC7702", () => {
     const setupTests = deployments.createFixture(async ({ deployments }) => {
         await deployments.fixture();
         const [deployer, relayer, delegator] = await ethers.getSigners();
@@ -41,8 +40,23 @@ describe("FallbackHandler", () => {
         };
     });
 
-    describe("Authorize Tx", function () {
-        it.only("Give authority to Safe", async () => {
+    const assertEmptyAccountStorage = async (account: AddressLike) => {
+        expect(await ethers.provider.getStorage(account, FALLBACK_HANDLER_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
+        expect(await ethers.provider.getStorage(account, GUARD_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
+
+        // Singleton address
+        expect(await ethers.provider.getStorage(account, 0)).to.equal(ethers.ZeroHash);
+        // Owner count
+        expect(await ethers.provider.getStorage(account, 3)).to.equal(ethers.ZeroHash);
+        // Threshold
+        expect(await ethers.provider.getStorage(account, 4)).to.equal(ethers.ZeroHash);
+
+        expect(await readModuleStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.ZeroHash);
+        expect(await readOwnerStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.ZeroHash);
+    };
+
+    describe("Test SafeERC7702Proxy", function () {
+        it("Give authority to SafeERC7702Proxy", async () => {
             const { safeSingleton, fallbackHandler, relayer, deployer, delegator, safeModuleSetup, safeERC7702ProxyFactory } =
                 await setupTests();
             const pkDelegator = process.env.PK3 || "";
@@ -75,7 +89,10 @@ describe("FallbackHandler", () => {
             const encodedSignedTx = await getSignedTransaction(ethers.provider, relayerSigningKey, authorizationList);
 
             const isAlreadyDelegated = await isAccountDelegatedToAddress(ethers.provider, await delegator.getAddress(), authAddress);
-            if (isAlreadyDelegated) console.log("Account already delegated to Safe. Resubmitting authorization.");
+            if (isAlreadyDelegated) {
+                console.log("Account already delegated to Safe Proxy. Returning");
+                return;
+            }
 
             const response = await ethers.provider.send("eth_sendRawTransaction", [encodedSignedTx]);
             console.log("Set Auth transaction hash", response);
@@ -93,10 +110,24 @@ describe("FallbackHandler", () => {
             expect(txSetupReceipt?.status === 1, "Transaction failed");
 
             const account = await delegator.getAddress();
-            await printAccountStorage(ethers.provider, account, await safeSingleton.getAddress());
+            console.log("ethers.zeroPadValue(fallbackHandlerAddress, 32): ", ethers.zeroPadValue(fallbackHandlerAddress, 32));
+            expect(await ethers.provider.getStorage(account, FALLBACK_HANDLER_STORAGE_SLOT)).to.equal(
+                ethers.zeroPadValue(fallbackHandlerAddress, 32),
+            );
+            expect(await ethers.provider.getStorage(account, GUARD_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
+            // Singleton address
+            expect(await ethers.provider.getStorage(account, 0)).to.equal(ethers.zeroPadValue(await safeSingleton.getAddress(), 32));
+            // Owner count
+            expect(await ethers.provider.getStorage(account, 3)).to.equal(ethers.zeroPadValue("0x01", 32));
+            // Threshold
+            expect(await ethers.provider.getStorage(account, 4)).to.equal(ethers.zeroPadValue("0x01", 32));
+            expect(await readModuleStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(
+                ethers.zeroPadValue(fallbackHandlerAddress, 32),
+            );
+            expect(await readOwnerStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.zeroPadValue(owners[0], 32));
         });
 
-        it.skip("Revoke authority to Safe and clear storage", async () => {
+        it("Revoke authority and clear storage", async () => {
             const { relayer, delegator, clearStorageHelper } = await setupTests();
 
             const pkDelegator = process.env.PK3 || "";
@@ -112,9 +143,7 @@ describe("FallbackHandler", () => {
             const authorizationList = getAuthorizationList(chainId, authNonce, pkDelegator, authAddress);
             let encodedSignedTx = await getSignedTransaction(ethers.provider, relayerSigningKey, authorizationList);
 
-            console.log("Sending transaction to redelegate to ClearStorageHelper");
             const response = await ethers.provider.send("eth_sendRawTransaction", [encodedSignedTx]);
-            console.log("Waiting for transaction confirmation");
             console.log("Transaction hash", response);
             const txReceipt = await (await ethers.provider.getTransaction(response))?.wait();
             expect(txReceipt?.status === 1, "Transaction failed");
@@ -122,101 +151,65 @@ describe("FallbackHandler", () => {
             const codeAtEOA = await ethers.provider.getCode(await delegator.getAddress());
             expect(codeAtEOA).to.equal(ethers.concat([ACCOUNT_CODE_PREFIX, await clearStorageHelper.getAddress()]));
 
-            console.log("Clearing Safe Account storage");
             const clearAccountStorage = await ethers.getContractAt("ClearStorageHelper", await delegator.getAddress());
             const txClearStorageResponse = await clearAccountStorage.connect(relayer).clearSafeStorage();
             await txClearStorageResponse.wait();
 
             const account = await delegator.getAddress();
-
-            expect(await ethers.provider.getStorage(account, FALLBACK_HANDLER_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
-
-            expect(await ethers.provider.getStorage(account, GUARD_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
-
-            // Singleton address
-            expect(await ethers.provider.getStorage(account, 0)).to.equal(ethers.ZeroHash);
-            // Owner count
-            expect(await ethers.provider.getStorage(account, 3)).to.equal(ethers.ZeroHash);
-            // Threshold
-            expect(await ethers.provider.getStorage(account, 4)).to.equal(ethers.ZeroHash);
-
-            expect(await readModuleStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.ZeroHash);
-            expect(await readOwnerStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.ZeroHash);
+            await assertEmptyAccountStorage(account);
         });
 
-        it.skip("Update fallback handler", async () => {
-            const { fallbackHandler, relayer, deployer, delegator } = await setupTests();
-            const newOwners = [deployer];
-            const safe = await getSafeAtAddress(await delegator.getAddress());
-            const data = safe.interface.encodeFunctionData("setFallbackHandler", [await fallbackHandler.getAddress()]);
-            const txResponse = await execTransaction(relayer, newOwners, safe, await safe.getAddress(), "0", data, "0");
-            const txReceipt = await txResponse.wait();
-            expect(txReceipt !== null && txReceipt.status === 1, "Transaction failed");
-        });
+        it("Clear storage using onRedelegation()", async () => {
+            const { safeSingleton, fallbackHandler, relayer, deployer, delegator, safeModuleSetup, safeERC7702ProxyFactory } =
+                await setupTests();
+            const pkDelegator = process.env.PK3 || "";
+            const pkRelayer = process.env.PK2 || "";
 
-        it.skip("Enable module", async () => {
-            const { fallbackHandler, relayer, deployer, delegator } = await setupTests();
-            const newOwners = [deployer];
-            const safe = await getSafeAtAddress(await delegator.getAddress());
+            const delegatorWallet = new ethers.Wallet(pkDelegator, hre.ethers.provider);
+            const relayerSigningKey = new SigningKey(pkRelayer);
 
-            expect(await safe.isModuleEnabled(await fallbackHandler.getAddress())).to.be.false;
-            const data = safe.interface.encodeFunctionData("enableModule", [await fallbackHandler.getAddress()]);
-            const txResponse = await execTransaction(relayer, newOwners, safe, await safe.getAddress(), "0", data, "0");
-            const txReceipt = await txResponse.wait();
-            expect(txReceipt !== null && txReceipt.status === 1, "Transaction failed");
-            const log = txReceipt?.logs.find((log: any) => log.address === safe);
-            expect(log && fallbackHandler.interface.parseLog(log)?.name === "EnabledModule");
-        });
+            const account = await delegatorWallet.getAddress();
+            expect(account).to.equal(await delegator.getAddress());
 
-        it.skip("Get fallback handler", async () => {
-            const { fallbackHandler, delegator } = await setupTests();
-            const safe = await getSafeAtAddress(await delegator.getAddress());
+            const chainId = (await ethers.provider.getNetwork()).chainId;
+            const authNonce = BigInt(await delegatorWallet.getNonce());
 
-            expect(await safe.getStorageAt(FALLBACK_HANDLER_STORAGE_SLOT, 1)).to.eq(
-                "0x" + (await fallbackHandler.getAddress())?.slice(2).toLowerCase().padStart(64, "0"),
-            );
-        });
-
-        it.skip("Clear Safe storage", async () => {
-            const { fallbackHandler, relayer, deployer, delegator, safeSingleton } = await setupTests();
+            // Deploy SafeProxy
             const owners = [deployer];
+            const ownerAddresses = await Promise.all(owners.map(async (owner): Promise<string> => await owner.getAddress()));
+            const fallbackHandlerAddress = await fallbackHandler.getAddress();
+            const setupData = getSetupData(
+                ownerAddresses,
+                1,
+                await safeModuleSetup.getAddress(),
+                [fallbackHandlerAddress],
+                fallbackHandlerAddress,
+            );
 
-            const safe = await getSafeAtAddress(await delegator.getAddress());
+            const proxyAddress = await calculateProxyAddress(safeERC7702ProxyFactory, await safeSingleton.getAddress(), setupData, 0);
+            const isContract = (await ethers.provider.getCode(proxyAddress)) === "0x" ? false : true;
+
+            if (!isContract) {
+                await safeERC7702ProxyFactory.connect(deployer).createProxyWithNonce(await safeSingleton.getAddress(), setupData, 0);
+            }
+
+            const authorizationList = getAuthorizationList(chainId, authNonce, pkDelegator, proxyAddress);
+            const encodedSignedTx = await getSignedTransaction(ethers.provider, relayerSigningKey, authorizationList);
+
+            const response = await ethers.provider.send("eth_sendRawTransaction", [encodedSignedTx]);
+            const txSetupDataReceipt = await (await ethers.provider.getTransaction(response))?.wait();
+            expect(txSetupDataReceipt?.status === 1, "Transaction failed");
+            const setupTxResponse = await relayer.sendTransaction({ to: account, data: setupData });
+            const txSetupReceipt = await setupTxResponse.wait();
+            expect(txSetupReceipt?.status === 1, "Transaction failed");
+
+            const safe = await getSafeAtAddress(account);
             const data = fallbackHandler.interface.encodeFunctionData("onRedelegation", []);
-            const txResponse = await execTransaction(relayer, owners, safe, await safe.getAddress(), "0", data, "0");
+            const txResponse = await execTransaction(relayer, owners, safe, account, "0", data, "0");
             const txReceipt = await txResponse.wait();
             expect(txReceipt !== null && txReceipt.status === 1, "Transaction failed");
 
-            const account = await delegator.getAddress();
-
-            expect(await ethers.provider.getStorage(account, FALLBACK_HANDLER_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
-
-            expect(await ethers.provider.getStorage(account, GUARD_STORAGE_SLOT)).to.equal(ethers.ZeroHash);
-
-            // Singleton address
-            expect(await ethers.provider.getStorage(account, 0)).to.equal(ethers.ZeroHash);
-            // Owner count
-            expect(await ethers.provider.getStorage(account, 3)).to.equal(ethers.ZeroHash);
-            // Threshold
-            expect(await ethers.provider.getStorage(account, 4)).to.equal(ethers.ZeroHash);
-
-            expect(await readModuleStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.ZeroHash);
-            expect(await readOwnerStorageSlot(ethers.provider, account, SENTINEL_ADDRESS)).to.equal(ethers.ZeroHash);
+            await assertEmptyAccountStorage(account);
         });
     });
 });
-
-const printAccountStorage = async (provider: Provider, account: AddressLike, safeSingleton: AddressLike | null) => {
-    await readStorage(provider, account);
-    console.log("account: ", account.toString());
-    console.log("code: ", await provider.getCode(account));
-    if (safeSingleton && (await provider.getCode(account)) === ethers.concat([ACCOUNT_CODE_PREFIX, safeSingleton.toString()])) {
-        const safe = await getSafeAtAddress(account.toString());
-        console.log("owners: ", await safe.getOwners());
-        console.log("threshold: ", await safe.getThreshold());
-        console.log("modules: ", await safe.getModulesPaginated("0x0000000000000000000000000000000000000001", 10));
-    }
-
-    console.log("module pointer at sentinel address: ", await readModuleStorageSlot(provider, account, SENTINEL_ADDRESS));
-    console.log("owner pointer at sentinel address: ", await readOwnerStorageSlot(provider, account, SENTINEL_ADDRESS));
-};
