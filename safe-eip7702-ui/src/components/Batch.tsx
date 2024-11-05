7981d15bcbfa78281a1e4e87b22f8abae26814f0
Akshay Patel <akshay.ap95@gmail.com>
2024-11-05 16:19:36 +0100

    Add batching for ithaca network

--- safe-eip7702-ui/src/components/Batch.tsx
+++ safe-eip7702-ui/src/components/Batch.tsx
@@ -1,30 +1,88 @@
 import React, { useContext, useState } from 'react';
-import { Button, Typography, TextField, IconButton, Card, CardContent, CardActions } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { createPimlicoClient } from "permissionless/clients/pimlico";
+import { Button, Typography, TextField, IconButton, Card, CardContent, CardActions, Dialog, DialogContent, DialogTitle, CircularProgress, Alert } from '@mui/material';
 import Grid from "@mui/material/Grid2";
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import { WalletContext } from '../context/WalletContext';
-import { FEATURES } from '../safe-eip7702-config/config';
+import { FEATURES, safeEIP7702Config } from '../safe-eip7702-config/config';
+import { createPublicClient, Hex, http, isAddress, isHex, zeroAddress } from 'viem';
+import { toSafeSmartAccount } from 'permissionless/accounts';
+import { createSmartAccountClient } from 'permissionless';
+import { odysseyTestnet } from "viem/chains"
+import { UserOperationCall } from 'viem/account-abstraction';
 
-interface Transaction {
-  to: string;
-  value: bigint;
-  data: string;
+enum BatchError {
+  SAFE_OWNER_NOT_CONNECTED = "Safe owner not connected",
+  MISSING_PIMLICO_API_KEY = "Missing Pimlico API Key"
 }
 
 const Batch: React.FC = () => {
-  const {features} = useContext(WalletContext)!;
-  const [transactions, setTransactions] = useState<Transaction[]>([{ to: '', value: BigInt(0), data: '0x' }]);
+  const { features, chainId, publicClient, account, safeStorage } = useContext(WalletContext)!;
+  const [transactions, setTransactions] = useState<UserOperationCall[]>([{ to: zeroAddress, value: BigInt(0), data: '0x' }]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [errors, setErrors] = useState<BatchError[]>([]);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const [transactionUrl, setTransactionUrl] = useState<string | null>(null);
 
-  const handleExecute = async() => {
-      setLoading(true);
-      setLoading(false);
+  const handleExecute = async () => {
+    setLoading(true);
+    setDialogOpen(true);
+
+    const pimlicoApiKey = import.meta.env.VITE_PIMLICO_API_KEY as Hex | undefined
+    if (!pimlicoApiKey) {
+      setErrors([...errors, BatchError.MISSING_PIMLICO_API_KEY]);
+      throw new Error("PIMLICO_API_KEY is required")
+    }
+
+    const pimlicoUrl = `https://api.pimlico.io/v2/${chainId}/rpc?apikey=${pimlicoApiKey}`
+
+    const pimlicoClient = createPimlicoClient({
+      transport: http(pimlicoUrl),
+    })
+
+    const publicClient = createPublicClient({
+      chain: odysseyTestnet,
+      transport: http("https://odyssey.ithaca.xyz"),
+    })
+
+    if (safeStorage && safeStorage.owners && account && safeStorage.owners.includes(account.address) && safeStorage.threshold === 1n) {
+      const safeAccount = await toSafeSmartAccount({
+        address: account?.address,
+        owners: [account],
+        client: publicClient,
+        version: "1.4.1",
+      })
+
+      const smartAccountClient = createSmartAccountClient({
+        account: safeAccount,
+        paymaster: pimlicoClient,
+        bundlerTransport: http(pimlicoUrl),
+        userOperation: {
+          estimateFeesPerGas: async () => (await pimlicoClient.getUserOperationGasPrice()).fast,
+        },
+      })
+
+      const userOperationHash = await smartAccountClient.sendUserOperation({
+        calls: transactions
+      })
+
+      const { receipt } = await smartAccountClient.waitForUserOperationReceipt({
+        hash: userOperationHash,
+      })
+
+      const url = `${safeEIP7702Config[chainId].explorer}/tx/${receipt.transactionHash}`;
+      setTransactionUrl(url);
+      console.log(`UserOperation included: ${url}`);
+    } else {
+      console.error(`Safe owner for account [${account?.address}] is not the connected account`)
+    }
+
+    setLoading(false);
   }
 
   const handleAddTransaction = () => {
-    setTransactions([...transactions, { to: '', value: BigInt(0), data: '0x' }]);
+    setTransactions([...transactions, { to: zeroAddress, value: BigInt(0), data: '0x' }]);
   };
 
   const handleRemoveTransaction = (index: number) => {
@@ -32,30 +90,22 @@ const Batch: React.FC = () => {
     setTransactions(newTransactions);
   };
 
-  const handleTransactionChange = (index: number, field: keyof Transaction, value: string) => {
+  const handleTransactionChange = (index: number, field: keyof UserOperationCall, value: string) => {
     const newTransactions = [...transactions];
     if (field === 'value') {
       newTransactions[index][field] = BigInt(value);
     } else {
-      newTransactions[index][field] = value;
+      newTransactions[index][field] = value as `0x${string}`;
     }
     setTransactions(newTransactions);
-  };
-
-  const isValidAddress = (address: string) => {
-    return /^0x[a-fA-F0-9]{40}$/.test(address);
-  };
-
-  const isValidHex = (data: string) => {
-    return /^0x[a-fA-F0-9]*$/.test(data);
   };
 
   return (
     <Grid container spacing={2}>
       <Grid size={12}>
-      <Typography variant="h1" align="left" fontSize={[44, null, 52]} sx={{ marginTop: 5 }}>
-        Batch and relay
-      </Typography>
+        <Typography variant="h1" align="left" fontSize={[44, null, 52]} sx={{ marginTop: 5 }}>
+          Batch and relay
+        </Typography>
       </Grid>
 
       {transactions.map((transaction, index) => (
@@ -68,8 +118,8 @@ const Batch: React.FC = () => {
                     label="To"
                     value={transaction.to}
                     onChange={(e) => handleTransactionChange(index, 'to', e.target.value)}
-                    error={!isValidAddress(transaction.to)}
-                    helperText={!isValidAddress(transaction.to) ? 'Invalid address' : ''}
+                    error={!isAddress(transaction.to)}
+                    helperText={!isAddress(transaction.to) ? 'Invalid address' : ''}
                     fullWidth
                   />
                 </Grid>
@@ -77,7 +127,7 @@ const Batch: React.FC = () => {
                   <TextField
                     label="Value"
                     type="number"
-                    value={transaction.value.toString()}
+                    value={transaction.value?.toString()}
                     onChange={(e) => handleTransactionChange(index, 'value', e.target.value)}
                     fullWidth
                   />
@@ -87,8 +137,8 @@ const Batch: React.FC = () => {
                     label="Data"
                     value={transaction.data}
                     onChange={(e) => handleTransactionChange(index, 'data', e.target.value)}
-                    error={!isValidHex(transaction.data)}
-                    helperText={!isValidHex(transaction.data) ? 'Invalid hex data' : ''}
+                    error={!isHex(transaction.data)}
+                    helperText={!isHex(transaction.data) ? 'Invalid hex data' : ''}
                     fullWidth
                   />
                 </Grid>
@@ -117,13 +167,38 @@ const Batch: React.FC = () => {
         <Button
           fullWidth
           variant="contained"
-          onClick={() => handleExecute()}
-          disabled={!features.includes(FEATURES.SUPPORT_4337) || loading}
+          onClick={async () => await handleExecute()}
+          disabled={!features.includes(FEATURES.SUPPORT_4337) || loading || !(safeStorage && safeStorage.owners && account && safeStorage.owners.includes(account.address) && safeStorage.threshold === 1n)}
           style={{ marginTop: '20px' }}
         >
           Execute
         </Button>
       </Grid>
+
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <DialogTitle>{loading ? "Processing" : <Alert severity='success'>Finished execution</Alert>}
+        </DialogTitle>
+        <DialogContent>
+          <Grid container justifyContent="center" alignItems="center" direction="column" spacing={2}>
+            {loading && (
+              <Grid>
+                <CircularProgress />
+              </Grid>
+            )}
+            {!loading && transactionUrl && (
+              <Grid>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => window.open(transactionUrl, '_blank')}
+                >
+                  View Transaction
+                </Button>
+              </Grid>
+            )}
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </Grid>
   );
 };
--- safe-eip7702-ui/src/context/WalletContext.tsx
+++ safe-eip7702-ui/src/context/WalletContext.tsx
@@ -1,8 +1,10 @@
 import React, { createContext, useState, ReactNode, useEffect } from 'react';
-import { isHex, PrivateKeyAccount } from 'viem'; // Import viem library for validation
+import { Address, createPublicClient, getContract, http, isAddress, isAddressEqual, isHex, PrivateKeyAccount, PublicClient } from 'viem'; // Import viem library for validation
 import { privateKeyToAccount } from 'viem/accounts';
 import { Authorization } from 'viem/experimental';
 import { defaultChainId, FEATURES, safeEIP7702Config } from '../safe-eip7702-config/config';
+import { readStorage, SafeStorage, SENTINEL_ADDRESS } from '../utils/storageReader';
+import safeArtifact from "../safe-eip7702-config/artifact/Safe.json";
 
 interface WalletContextType {
   privateKey: `0x${string}` | undefined;
@@ -15,6 +17,10 @@ interface WalletContextType {
   chainId: number;
   setChainId: (chainId: number) => void;
   features: FEATURES[];
+  publicClient: PublicClient;
+  loading: boolean,
+  safeStorage: SafeStorage | undefined,
+  accountCode: string | undefined
 }
 
 // Create the context
@@ -28,6 +34,12 @@ export const WalletProvider: React.FC<{ children: ReactNode }> = ({ children }) 
   const [authorizations, setAuthorizations] = useState<Authorization[]>([]);
   const [chainId, setChainId] = useState<number>(defaultChainId);
   const [features, setFeatures] = useState<FEATURES[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [safeStorage, setSafeStorage] = useState<SafeStorage>();
+  const [accountCode, setAccountCode] = useState<string>();
+  const [publicClient, setPublicClient] = useState<PublicClient>(createPublicClient({
+    transport: http(safeEIP7702Config[chainId].rpc),
+  }));
 
   // Function to validate the private key
   const validatePrivateKey = (key: `0x${string}` | undefined) => {
@@ -40,13 +52,56 @@ export const WalletProvider: React.FC<{ children: ReactNode }> = ({ children }) 
     setPrivateKey(key as `0x${string}`);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     setFeatures(safeEIP7702Config[chainId].features)
+    setPublicClient(createPublicClient({
+      transport: http(safeEIP7702Config[chainId].rpc),
+    }));
   }, [chainId]);
+
+  const loadStorage = async () => {
+    const accountAddress = account.address;
+    if (!account || !isAddress(accountAddress)) {
+      setAccountCode(undefined);
+      setSafeStorage(undefined);
+      return;
+    };
+    setLoading(true);
+    let storage = await readStorage(publicClient, accountAddress);
+    console.log(`Storage values for account [${accountAddress}]:`, storage);
+    const accountCode = await publicClient.getCode({ address: accountAddress });
+
+    if (isAddressEqual(storage.singleton, safeEIP7702Config[chainId].addresses.safeSingleton)) {
+
+      const contract = getContract({
+        address: accountAddress,
+        abi: safeArtifact.abi,
+        client: publicClient,
+      });
+
+      storage.owners = ((await contract.read.getOwners()) as Address[]) || [];
+      // Assumes that there are no more than 10 modules enabled.
+      const modulesResult = (await contract.read.getModulesPaginated([SENTINEL_ADDRESS, 10])) as Address[][];
+      if (modulesResult && modulesResult.length > 0) {
+        storage.modules = modulesResult[0];
+      }
+    }
+
+    setAccountCode(accountCode);
+    setSafeStorage(storage);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await loadStorage();
+    })();
+  }, [account]);
+
 
 
   return (
-    <WalletContext.Provider value={{ features, chainId, setChainId, authorizations, setAuthorizations, privateKey, setPrivateKey: validatePrivateKey, isPrivateKeyValid, account, setAccount }}>
+    <WalletContext.Provider value={{ safeStorage, accountCode, loading, publicClient, features, chainId, setChainId, authorizations, setAuthorizations, privateKey, setPrivateKey: validatePrivateKey, isPrivateKeyValid, account, setAccount }}>
       {children}
     </WalletContext.Provider>
   );
